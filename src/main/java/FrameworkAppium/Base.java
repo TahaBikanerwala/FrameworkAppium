bf909127396f977d8c17d51d250d00188c8dc0b6
Incubyte-Taha <incubyte-taha@pop-os.localdomain>
2022-06-04 01:13:28 +0530

    Basic POM and BDD implemented - working code

--- src/main/java/FrameworkAppium/Base.java
+++ src/main/java/FrameworkAppium/Base.java
@@ -11,11 +11,23 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
 
 public class Base {
 
+
+	protected static AppiumDriverLocalService service;
+
+	public static AppiumDriverLocalService startServer(){
+		service = AppiumDriverLocalService.buildDefaultService();
+		service.start();
+		return service;
+	}
+
+	// public void stopServer(){
+	// 	service.stop();
+	// }
 	public static AndroidDriver capabilities() throws IOException {
-		
 		
 		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"/src/main/java/FrameworkAppium/global.properties");
 		Properties prop = new Properties();
@@ -25,7 +37,7 @@ public class Base {
 		File appDir = new File("src");
 		File app = new File(appDir,(String) prop.get("AppNameKey"));
 		DesiredCapabilities cap = new DesiredCapabilities();
-		
+		// /home/incubyte-taha/Android/Sdk/emulator
 		String device = (String) prop.get("DeviceName");
 		cap.setCapability("deviceName", device);
 		
--- src/test/java/stepdefinitions/SampleTest.java
+++ src/test/java/stepdefinitions/SampleTest.java
@@ -2,32 +2,58 @@ package stepdefinitions;
 
 import java.io.IOException;
 
-import org.junit.Test;
-
 import FrameworkAppium.Base;
-import PageObjects.HomePage;
-import PageObjects.Preferences;
+import PageObjects.HelpPage;
+import PageObjects.JoinPage;
+import PageObjects.SignInPage;
+import PageObjects.SignUpPage;
 import io.appium.java_client.android.AndroidDriver;
+import io.cucumber.java.BeforeAll;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 
+
 public class SampleTest extends Base{
 	
-	static HomePage homePage;
-	static Preferences prefs;
+	static SignInPage signInPage;
+	static JoinPage joinInPage;
+	static SignUpPage signUpPage;
+	static HelpPage helpPage;
 	AndroidDriver driver;
+	
+
+	@BeforeAll
+	public static void before_all(){
+		// service = startServer();
+	}
 	
 	@Given("User launches the app")
 	public void user_launches_the_app() throws IOException {
 		 driver = capabilities();
-		 homePage = new HomePage(driver);
-		 homePage.clickpreferences();
-		 
+		 signInPage = new SignInPage(driver);
+
+		//  signInPage.typeEmail("lawson_8857.hammes@gmail.com");
+		//  signInPage.typePassword("foobarbazz");
+		//  signInPage.signInButtonClick();
 	}
 	@Then("HomeScreen should be displayed")
-	public void home_screen_should_be_displayed() {
-		prefs = new Preferences(driver);
-		prefs.clickpreferences_dep();
+	public void home_screen_should_be_displayed() throws InterruptedException {
+		Thread.sleep(2000);
+		joinInPage = signInPage.signUpButtonClick();
+		Thread.sleep(2000);
+		signUpPage = joinInPage.joinOnlyMeClick();
+		Thread.sleep(2000);
+		signUpPage.userNameEnter("Taha");
+		Thread.sleep(2000);
+		signUpPage.lastNameEnter("Bikanerwala");
+		Thread.sleep(2000);
+		signUpPage.emailEnter("dummy_mail@dummy.com");
+		Thread.sleep(2000);
+		signUpPage.termsCheckBoxClick();
+		helpPage = signUpPage.helpButtonClick();
+		Thread.sleep(2000);
+		
+
 	}
 
 
